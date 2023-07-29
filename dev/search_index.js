dfdb738876d01b4038d19a35e0401e474b6a3453
Documenter.jl <documenter@juliadocs.github.io>
2023-07-29 01:36:01 +0000

    build based on 5349c12

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = ShoppingCarts","category":"page"},{"location":"#ShoppingCarts","page":"Home","title":"ShoppingCarts","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for ShoppingCarts.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [ShoppingCarts]","category":"page"}]
+}
